1c80bbb4da1c96c6d3df37ee9bc681bf96ab1428
kavithaqueen <kaviselvam730@gmail.com>
2022-10-11 13:13:39 -0700

    employee details added successfully

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,48 @@
+package org.emp;
+
+public class Employee {
+
+	private void empId() {
+		System.out.println("1045");
+	}
+	
+	private void empDob() {
+		System.out.println("08/02/2002");
+	}
+	private void empPhone() {
+		System.out.println("97106534");
+	}
+	private void empEmail() {
+		System.out.println("kowsalyaselvam82002@gmail.com");
+	}
+	private void empAddress() {
+		System.out.println("3/64,East Street,Eas Rajapalayam");
+	}
+	
+	public static void main (String[]args) {
+		Employee s=new Employee();
+		s.empId();
+		s.empDob();
+		s.empPhone();
+		s.empEmail();
+		s.empAddress();  
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+}
